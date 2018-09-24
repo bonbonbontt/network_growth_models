d0cb41ccecb107703a064cd4d50b4b332dd1f3a4
hrshah4 <hrshah4@illinois.edu>
2018-09-24 16:12:36 -0500

    Update path length script

--- apl.py
+++ apl.py
@@ -5,64 +5,41 @@ import igraph as ig
 import netprop
 import utils
 
-def get_input():
-	return {
-		'graph_path': raw_input('enter path to pickled graph: '),
-		'out_path': raw_input('enter path to write path lengths: ')
-	}
+"""
+This script takes two arguments
+- graph path (path to pickled graph)
+- out path (path to write path lengths)
+"""
 
-def run():
+def run(g, fname, update_every=None):
 	# load input
-	input = get_input()
-	g = input['graph'] = ig.Graph.Read_Pickle(input['graph_path'])
-	f_out = open(input['out_path'])
-
+	f_out = open(fname, 'a')
 	g = g.as_undirected()
+
 	nids = g.vs.indices[:]
+	random.shuffle(nids)
+	sampled_nids = set()
 
-	# run
 	while (len(sampled_nids) < len(g.vs)):
+		# sample a node, compute its lengths
 		nid = nids.pop()
-		pl = get_node_path_length(g, nid)
+		pl = netprop.get_node_path_length(g, nid)
 
+		# update file
+		for path_lenth, freq in pl.items():
+			f_out.write('{} {} {}\n'.format(nid, path_lenth, freq))
 
+		sampled_nids.add(nid)
 
+		if update_every and (len(sampled_nids) % update_every == 0):
+			print "{}/{} nodes done".format(len(sampled_nids), len(nids))
 
+	close(f_out)
+	return input
 
+if __name__ == '__main__':
+	assert len(sys.argv) == 3, "enter arguments"
+	_, graph_path, out_path = sys.argv
 
-
-# def get_graphs():
-# 	networks = {}
-# 	for gpath in paths:
-# 		fname = gpath.rsplit('/', 1)[-1].split('.')[0]
-# 		networks[fname] = ig.Graph.Read_Pickle(gpath)
-# 		print fname
-# 		print networks[fname].summary()
-# 	return networks
-#
-# def apl(graph, N, deg_mode = 'ALL'):
-# 	nids = np.random.choice(graph.vs.indices, size=N)
-# 	allnids = graph.vs.indices
-# 	lengths = np.array(graph.shortest_paths(source=nids, mode=deg_mode))
-#
-# 	# write distances to file
-# 	full_name = output_path + '/' + fname + '.txt'
-# 	f = open(full_name, 'w')
-# 	for row in range(len(nids)):
-# 		for col in range(len(allnids)):
-# 			f.write(str(nids[row]) + " " + str(allnids[col]) + " " + str(lengths[row][col]) + "\n")
-# 	f.close()
-#
-#
-# if __name__ == '__main__':
-# 	path = 'data/networks'
-# 	output_path = 'data/path_length_outputs'
-# 	paths = [os.path.join(path, f) for f in os.listdir(path) if not f.startswith('.')]
-# 	networks = get_graphs()
-# 	for fname in networks:
-# 		N = int(len(networks[fname].vs) * 0.01)
-# 		apl(networks[fname], N)
-#
-
-
-
+	g = ig.Graph.Read_Pickle(graph_path)
+	run(g, out_path, 100)
--- netprop.py
+++ netprop.py
@@ -9,11 +9,11 @@ import utils
 
 def get_node_path_length(graph, nid, deg_mode='ALL'):
     """compute distances to all nodes from nid"""
-    lengths = graph.as_undirected().shortest_paths(source=nid, mode=deg_mode)
+    lengths = graph.as_undirected().shortest_paths(source=nid, mode=deg_mode)[0]
     counter = Counter(lengths)
     counter.pop(np.inf, None); counter.pop(0, None);
     return dict(counter)
-    
+
 def get_degree_distribution(graph, deg_mode='IN'):
     return utils.get_discrete_distribution(graph.degree(mode=deg_mode))
 
